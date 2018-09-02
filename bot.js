01b1842a63d1a8a4bb590398adc648d3e5010df5
BotMinion <36266884+BotMinion@users.noreply.github.com>
2018-09-02 17:20:34 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ client.on('message', message => {
     if (message.content === '=트윗트윗') {
     	message.reply('https://twitter.com/');
     }
-    if (message.content === '=파새') {
+    if (message.content === '=파란새') {
     	message.reply('https://twitter.com/');
     }
     if (message.content === '=Blue Bird') {
